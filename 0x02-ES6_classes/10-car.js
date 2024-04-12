abf354cf526b2f4e5c0e6668d237ff2aa62a234c
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-04-13 00:44:37 +0100

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,9 +1,3 @@
-/* eslint-disable no-underscore-dangle */
-// eslint-disable-next-line no-underscore-dangle
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
@@ -11,11 +5,7 @@ export default class Car {
     this._color = color;
   }
 
-  static get [Symbol.species]() {
-    return this;
-  }
-
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
