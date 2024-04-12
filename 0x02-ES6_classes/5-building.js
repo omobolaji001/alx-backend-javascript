399268c7d73d868e6e342e42f3df21d378481a19
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-04-12 23:18:43 +0100

    Abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
