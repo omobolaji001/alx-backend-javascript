7694ccfa51051978a5787f2660d034b9d0098ccb
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-06-06 18:29:30 +0100

    Test calculateNumber

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,26 +5,26 @@ describe("calculateNumber", function() {
 
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUM", 1, 2);
-      expect(res).to.be.equal(3);
+      expect(res).to.equal(3);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-      expect(res).to.be.equal(-1);
+      expect(res).to.equal(-1);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-      expect(res).to.be.equal(2);
+      expect(res).to.equal(2);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 4, 2);
-      expect(res).to.be.equal(2);
+      expect(res).to.equal(2);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.7, 0);
-      expect(res).to.be.equal("Error");
+      expect(res).to.equal("Error");
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.4, 4.6);
-      expect(res).to.be.equal(0.2);
+      expect(res).to.equal(0.2);
     });
 });
